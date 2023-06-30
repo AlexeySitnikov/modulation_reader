fc4725c5189bc3170937a21a9dd514e29db5901c
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-06-30 14:54:50 +0300

    log out a full modulation file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css'
 
 function App() {
+  let arrayOfStrings = []
   const clickHandlerFileChange = (e) => {
-    const file = e.target.files[0]
     const reader = new FileReader()
-    reader.readAsText(file)
+    reader.readAsText(e.target.files[0])
     reader.onload = () => {
-      console.log(reader.result)
+      arrayOfStrings = reader.result
+        .replace(/[-]/g, '')
+        .split('\n')
+        .map((el) => el.trim())
+        .map((el) => el.replace(/\s\s+/g, ' '))
+      for (let i = 0; i < arrayOfStrings.length; i += 1) {
+        console.log(arrayOfStrings[i])
+      }
+
+      // console.log(arr[1].split(' '))
     }
   }
 
