844c04ccc18b35a1ffc524c268c0f9b37ac8ca93
AlexeySitnikov <alexey.sitnikov@bk.ru>
2023-10-06 15:42:40 +0300

    try to add points of modulation

--- src/components/ListOfValues.js
+++ src/components/ListOfValues.js
@@ -1,3 +1,6 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-mixed-operators */
+/* eslint-disable max-len */
 import { useDispatch, useSelector } from 'react-redux'
 import style from './styles.module.css'
 import { RowsOfValues } from './RowsOfValues'
@@ -90,6 +93,83 @@ export function ListOfValues({ listOfvariables }) {
     download(Z, 'Z.txt')
   }
 
+  const makeVaneModulation = () => {
+    const modulation = rows.map((el) => Number(el.element.split(' ')[5]))
+    modulation.push(modulation[modulation.length - 1])
+
+    const R_0 = rows.map((el) => Number(el.element.split(' ')[7]))
+    R_0.push(R_0[R_0.length - 1])
+
+    const Z = rows.map((el) => Number(el.element.split(' ')[22]))
+
+    const step = 0.1
+    let z_coor = 0
+    let t = 0.0
+    let tip = 0n
+    const f = []
+    let n = 1
+
+    while (t <= Z[0]) {
+      tip = ((R_0[0]
+        + (t / Z[0]) * (R_0[1] - R_0[0]))
+      * (1 - (0.5 * (modulation[0] + modulation[1])
+      + 0.5 * (modulation[0] - modulation[1])
+      * Math.cos(Math.PI * (t / Z[0])) - 1)
+      / (0.5 * (modulation[0] + modulation[1])
+      + 0.5 * (modulation[0] - modulation[1])
+      * (Math.cos(Math.PI * (t / Z[0])) + 1) * Math.cos(Math.PI * (t / Z[0])))))
+
+      f.push(`${Number.parseFloat(z_coor).toFixed(2)}\t${Number.parseFloat(tip).toFixed(5)}\n`)
+
+      t = Math.round(step * n * 100) / 100
+      if (t <= Z[0]) {
+        z_coor = Math.round(step * n * 100) / 100
+        n += 1
+      }
+    }
+
+    for (let i = 0; i < Z.length; i += 1) {
+      let n1 = 1
+      if ((i % 2) === 0) {
+        t = Math.round((Math.round((Z[i + 1] - Z[i]) * 100) / 100 - Math.round(t * 100) / 100) * 100 / 100)
+        while (t <= Math.round((Z[i + 1] - Z[i]) * 100) / 100) {
+          tip = (R_0[i + 1] + ((t / (Z[i + 1] - Z[i])) * (R_0[i + 2] - R_0[i + 1])))
+          * (1
+            + ((0.5 * (modulation[i + 1] + modulation[i + 2]) + 0.5 * (modulation[i + 1] - modulation[i + 2]) * Math.cos(Math.PI * t / (Z[i + 1] - Z[i])) - 1) / (0.5 * (modulation[i + 1] + modulation[i + 2]) + 0.5 * (modulation[i + 1] - modulation[i + 2]) * Math.cos(Math.PI * t / (Z[i + 1] - Z[i])) + 1)
+            ) * (Math.cos(Math.PI * t / (Z[i + 1] - Z[i]))))
+
+          f.push(`${Number.parseFloat(z_coor).toFixed(2)}\t${Number.parseFloat(tip).toFixed(5)}\n`)
+          t = Math.round(step * n1 * 100) / 100
+
+          if (t <= Math.round((Z[i + 1] - Z[i]) * 100) / 100) {
+            z_coor = Math.round(step * n * 100) / 100
+            n += 1
+            n1 += 1
+          }
+        }
+      } else {
+        t = Math.round((Z[i + 1] - Z[i]) * 100) / 100 - Math.round(t * 100) / 100
+        while (t <= Math.round((Z[i + 1] - Z[i]) * 100) / 100) {
+          tip = (R_0[i + 1] + ((t / (Z[i + 1] - Z[i])) * (R_0[i + 2] - R_0[i + 1])))
+          * (1
+            - ((0.5 * (modulation[i + 1] + modulation[i + 2]) + 0.5 * (modulation[i + 1] - modulation[i + 2]) * Math.cos(Math.PI * t / (Z[i + 1] - Z[i])) - 1) / (0.5 * (modulation[i + 1] + modulation[i + 2]) + 0.5 * (modulation[i + 1] - modulation[i + 2]) * Math.cos(Math.PI * t / (Z[i + 1] - Z[i])) + 1)
+            ) * (Math.cos(Math.PI * t / (Z[i + 1] - Z[i]))))
+
+          f.push(`${Number.parseFloat(z_coor).toFixed(2)}\t${Number.parseFloat(tip).toFixed(5)}\n`)
+          t = Math.round(step * n1 * 100) / 100
+          if (t <= Math.round((Z[i + 1] - Z[i]) * 100) / 100) {
+            z_coor = Math.round(step * n * 100) / 100
+            n += 1
+            n1 += 1
+          }
+        }
+      }
+    }
+    // console.log({ Z })
+    // console.log({ f })
+    download(f, 'f.txt')
+  }
+
   if (!rows) {
     return (
       <div>
@@ -134,6 +214,14 @@ export function ListOfValues({ listOfvariables }) {
           }}
         >
           Download Z
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            makeVaneModulation()
+          }}
+        >
+          Make vane modulation
         </button>
       </div>
       <RowsOfValues rows={rows} />
